ad052b2e8c7eaeba10a3290d19b11fd8c4ec8d48
doodlehaus <doodlehaus@gmail.com>
2011-12-11 14:44:05 -0600

    new app

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,17 @@
+class ChallengesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def accept
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vvv::Application.config.secret_token = '6b3854049789b01e3c83b6071777af001dd9401658e3d45a9de839e06b72ea2be568ea2cbe0a5f75494085fcae262fcaafc6c9c922821c99744ac34d6686e27e'
